6a694eef18e927addbc8273aaabda133fe5ec9e4
Rafael Gil <rafael@MacBook-Pro-de-Rafael.local>
2017-10-12 12:55:46 -0300

    fixed inser query

--- categories/index.js
+++ categories/index.js
@@ -19,9 +19,8 @@ class CategoriesController {
   }
 
   create(req, context) {
-    context.log(req);
     const queryParams = [req.body.name, req.body.description];
-    return DB.query('insert into categories (name, description) values ($1, $1) returning *', queryParams);
+    return DB.query('insert into categories (name, description) values ($1, $2) returning *', queryParams);
   }
 
   update(req) {
