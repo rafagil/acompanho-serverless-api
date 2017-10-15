e47fa1e5c5918b18d23dd2e2ab23eb70439ddf7e
Rafael Gil <rafael@MacBook-Pro-de-Rafael.local>
2017-10-15 18:37:04 -0200

    added query id verification

--- front-controller.js
+++ front-controller.js
@@ -16,25 +16,25 @@ module.exports = (controller) => {
     // Log.setLogger(context.log);
     switch (req.method) {
       case "POST":
-        if (req.params.id)
+        if (req.query.id ||req.params.id)
           res = error405(); //Não pode adicionar em um recurso
         else
           res = getResponse(controller, req, context, 'create');
         break;
       case "PUT":
-        if (req.params.id)
+        if (req.query.id ||req.params.id)
           res = getResponse(controller, req, context, 'update');
         else
           res = error405(); // Não pode atualizar sem id
         break;
       case "DELETE":
-        if (req.params.id)
+        if (req.query.id ||req.params.id)
           res = getResponse(controller, req, context, 'remove');
         else
           res = error405(); //Não pode deletar sem id
         break;
       case "GET":
-        if (req.params.id)
+        if (req.query.id ||req.params.id)
           res = getResponse(controller, req, context, 'find');
         else
           res = getResponse(controller, req, context, 'list');
